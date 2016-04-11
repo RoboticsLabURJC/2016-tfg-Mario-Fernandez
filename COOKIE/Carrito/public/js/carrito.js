891bfd46acb68ae5c3809e0efda34dba3f07de4e
graylinx <mariofg4@gmail.com>
2016-04-11 20:07:52 +0200

    version2

--- COOKIE/Carrito/public/js/carrito.js
+++ COOKIE/Carrito/public/js/carrito.js
@@ -1,13 +1,18 @@
 var user;
 console.log(document.cookie);
-  
 
-// Funciones que me permiten tratar la cookie 
+
+// Funciones que me permiten tratar la cookie
 function setCookie(name,compra){
       document.cookie = name + "=" + JSON.stringify(compra);
 }
 
-      
+function clearCookie (key) {
+    console.log("eliminarCookie: " + key);
+    return document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -16,7 +21,7 @@ function getCookie(cname) {
         var c = ca[i];
         //le quitamos el primer espacio que siempre lleva la cookie
         while (c.charAt(0)==' '){
-          c = c.substring(1); 
+          c = c.substring(1);
         }
         //nos quedamos con el producto(valor) de cada uno
         if (c.indexOf(name) == 0) {
@@ -27,14 +32,41 @@ function getCookie(cname) {
     return null;
 }
 
-/// Buscamos usuario para darle el contenido      
+/// Buscamos usuario para darle el contenido
 function checkCookie(){
       user = document.getElementById("name_user").value;
+      //products = valor de la clave user
       var products = getCookie(user);
       console.log("-->>> Buscamos contenido de "+user+"= "+JSON.parse(products));
       if (products != null){
         Show_Car(products);
-      }     
+      }
+}
+
+/// Damos las gracias por la compra y limpiamos la cookie
+function thanksCookie(){
+      console.log(document.cookie);
+      cookie = document.cookie.split("=");
+      user = cookie[0];
+      var products = getCookie(user);
+      console.log("-->>> Buscamos contenido de "+user+"= "+JSON.parse(products));
+      var carrito = JSON.parse(products);
+      console.log(carrito);
+      var carrito = carrito.carrito;
+      var out = "<table>";
+      for(i = 0; i < carrito.length; i++) {
+          out += "<tr><td>" +
+          "</td><td>" +
+          carrito[i].compra.nombre +
+          "</td><td>" +
+          carrito[i].compra.cantidad +
+          "</td></tr>";
+      }
+      out += "</table>";
+      document.getElementById("receive").innerHTML = "Thank you very much for this purchase: " + user;
+      document.getElementById("elementos").innerHTML = out;
+      clearCookie(user);
+    	console.log(">> Eliminamos en contenido de la cookie de "+user);
 }
 
 function Show_Car(content){
@@ -45,7 +77,7 @@ function Show_Car(content){
     console.log(carrito);
     var out = "<table>";
     for(i = 0; i < carrito.length; i++) {
-        out += "<tr><td>" + 
+        out += "<tr><td>" +
         user +":     " +
         "</td><td>" +
         carrito[i].compra.nombre +
@@ -88,19 +120,15 @@ function Añadir(id){
       var list =[obj];
       var car = {carrito:list};
       console.log("-->>> Añadimos primer producto: "+ articulo.nombre);
+      //añadimos a document.cookie cuya clave sea user el siguiente val
       setCookie(user,car);
       console.log(obj);
     }else{
       products = JSON.parse(content);
       console.log(products);
       var carrito = products.carrito;
-      Othermore(carrito, articulo);  
+      Othermore(carrito, articulo);
       setCookie(user,products);
     }
-  
+
 }
-
-
-
-      
-    
