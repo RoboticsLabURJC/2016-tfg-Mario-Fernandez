86fcf15b0140b3c1874b3a8bba679d257473c575
graylinx <mariofg4@gmail.com>
2016-04-11 20:06:59 +0200

    version2

--- COOKIE/Carrito/Server.js
+++ COOKIE/Carrito/Server.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 app.use(express.static('public'));
-app.use(cookieParser()); 
+app.use(cookieParser());
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
@@ -16,25 +16,27 @@ app.get('/', function(req, res){
 
 app.get('/clear', function(req, res){
 	res.clearCookie('undefined');
+  res.clearCookie('mario');
+  res.clearCookie('juan');
  	res.sendfile(__dirname + '/index.html');
 });
 
-  
+
 app.post('/',urlencodedParser,function(req,res){
 	var user = req.body.user;
 	console.log("Purchase of: "+user+":");
 	console.log(req.cookies);
 	var carrito= JSON.parse(req.cookies[user]);
+  console.log(carrito);
 	var compra = carrito.carrito;
 	for(var i=0;i < compra.length;i++){
 		var item  = compra[i].compra;
 		console.log(">> Article:"+item.nombre+";Unidades:"+item.cantidad);
 	}
-	res.clearCookie(user);
-	console.log("Clear_cookie of: "+user);
+  res.cookie(user,JSON.stringify(carrito),{encode: String});
 	res.sendfile(__dirname + '/Response.html');
 });
-  
+
 
 http.listen(3000, function(){
  	console.log('listening on *:3000');
