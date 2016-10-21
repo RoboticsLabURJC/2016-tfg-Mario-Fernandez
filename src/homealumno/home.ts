d945f1aab441dd350196a84cee83e3d79cbd1d24
Mario Fernandez <b.mario.fernandez@optivamedia.com>
2016-10-21 14:57:54 +0200

    comment

--- src/homealumno/home.ts
+++ src/homealumno/home.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AuthHttp } from 'angular2-jwt';
 import { DataAlumno } from './dataalumno';
 import { DataProfesor } from './dataprofesor';
+import { Observable, Observer } from 'rxjs';
 
 
 declare var google: any;
@@ -17,12 +18,13 @@ declare var google: any;
 export class Home {
   jwt: string;
   decodedJwt: DataAlumno;
-  
+
   profesores: DataProfesor[] = [];
   alumno = new DataAlumno('', '', new Date(''), '', {lat: 40.416775, lng: -3.7037901999999576});
 
   address : string = 'Madrid';
   curso = ['Primaria', 'ESO', 'Bachillerato', 'Universidad', 'F.P.', 'Examenes'];
+  numero: number[] = [];
 
   constructor(public router: Router, public http: Http, public authHttp: AuthHttp) {
     this.jwt = localStorage.getItem('id_token');
@@ -30,20 +32,40 @@ export class Home {
     console.log(this.decodedJwt);
   }
 
-  getcoors(address: string) {
-      console.log(address);
-      console.log('Getting Address - ', address);
-      let geocoder = new google.maps.Geocoder();
-      geocoder.geocode({ 'address': address }, (results, status) => {
-        if (status === google.maps.GeocoderStatus.OK) {
-          this.alumno.loc.lat = results[0].geometry.location.lat();
-          this.alumno.loc.lng = results[0].geometry.location.lng();
-        }else {
-            alert('Geocode was not successful for the following reason: ' + status);
+  initcoor(address: string){
+    this.getLatLan(address).
+      subscribe(
+        response => {
+          console.log(response);
+          console.log(this.alumno.loc);
+          this.alumno.loc = response;
+          console.log(this.alumno.loc);
+          //console.log(this.alumno.loc.lat);
+        },
+        error => {
+          alert(error.text());
+          console.log(error.text());
         }
-      });
+      );
   }
 
+  getLatLan(address: string) {
+        console.log('Getting Address - ', address);
+        let geocoder = new google.maps.Geocoder();
+        return Observable.create(observer => {
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                  let obj: Object= {lat: results[0].geometry.location.lat(),lng: results[0].geometry.location.lng() }
+                  observer.next(obj);
+                  observer.complete();
+                } else {
+                    console.log('Error - ', results, ' & Status - ', status);
+                    observer.next({});
+                    observer.complete();
+                }
+            });
+        })
+    }
   getallprof() {
     let url = 'http://localhost:3001/profesores';
     this.http.get(url).
