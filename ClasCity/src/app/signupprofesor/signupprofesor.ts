43115b88a884104be9a52344cd9593018da0803d
Mario <mariofg4@gmail.com>
2018-05-21 20:35:58 +0200

    App en pro

--- ClasCity/src/app/signupprofesor/signupprofesor.ts
+++ ClasCity/src/app/signupprofesor/signupprofesor.ts
@@ -11,7 +11,7 @@ import {FileSelectDirective,
         FileDropDirective,
         FileUploader} from 'ng2-file-upload/ng2-file-upload';
 
-const URL_SERVER ='http://ec2-54-84-25-113.compute-1.amazonaws.com/app';
+const URL_SERVER ='https://54.84.25.113/app';
 
 @Component({
   selector: 'signupprofesor',
